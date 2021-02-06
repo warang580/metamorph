75a555798116128b90c7268488a62d5d5aab7829
Sacha WILT <wsacha67@gmail.com>
2021-02-06 22:47:18 +0100

    feat: forced Inspector for SceneManager

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <SceneManager />
+  <Inspector for="SceneManager" />
 </template>
 
 <script>
 import SceneManager from './components/SceneManager.vue'
+import Inspector from './components/Inspector.vue'
 
 export default {
   components: {
     SceneManager,
+    Inspector,
   },
 }
 </script>
